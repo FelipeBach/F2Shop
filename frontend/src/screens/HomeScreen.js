31608b2be25a3cb78d44653e3f8ba2b25259443f
Felipe Bach <felipe.bach@meta.com.br>
2022-04-18 17:22:44 -0300

    added reducer and reducer logger to manage and log state changes

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,43 +1,74 @@
-//import data from "../data";
-import React, {Link} from 'react-router-dom';
+import React, { Link } from 'react-router-dom';
 import axios from 'axios';
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
+import logger from "use-reducer-logger";
 
-function HomeScreen(){
-    const [products, setProducts] = useState([]);
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'FETCH_REQUEST':
+            return { ...state, loading: true };
+        case 'FETCH_SUCCESS':
+            return { ...state, products: action.payload, loading: false };
+        case 'FETCH_FAIL':
+            return { ...state, loading: false, error: action.payload };
+        default:
+            return state;
+    }
+};
+
+function HomeScreen() {
+    const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+        products: [],
+        loading: true,
+        error: '',
+    });
+
+    //const [products, setProducts] = useState([]);
     useEffect(() => {
         const fetchData = async () => {
-            const result = await axios.get('/api/products')
-            setProducts(result.data);
+            dispatch({ type: 'FETCH_REQUEST' });
+            try {
+                const result = await axios.get('/api/products');
+                dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+            } catch (err) {
+                dispatch({ type: 'FETCH_FAIL', payload: err.message });
+            }
+
+            //setProducts(result.data);
         };
         fetchData();
-    }, []);    
+    }, []);
     return (
         <div>
             <div className='row center'>
-                {
-                products.map((product) =>(
-                    <div key={product.slug} className="card">
-                    <Link to={`/product/${product.slug}`}>
-                        <img 
-                            className='medium' 
-                            src={product.image} 
-                            alt={product.name}>
-                        </img>                        
-                    </Link>
-                    <div className="card body">
+                {loading ? (<div>Loading...</div>
+                )
+                :error ? (<div>{error}</div>
+                )
+                :(
+                    products.map((product) => (
+                    <div className="card" key={product.slug}>
                         <Link to={`/product/${product.slug}`}>
-                            <h2>{product.name}</h2>
+                            <img
+                                className='medium'
+                                src={product.image}
+                                alt={product.name}>
+                            </img>
                         </Link>
-                    </div>
-                    <div className="price">R${product.price}</div>
-                    <button>Adicionar ao carrinho</button>                                              
-                </div>      
+                        <div className="card body">
+                            <Link to={`/product/${product.slug}`}>
+                                <h2>{product.name}</h2>
+                            </Link>
+                        </div>
+                        <div className="price">R${product.price}</div>
+                        <button>Adicionar ao carrinho</button>
+                    </div>)
                 ))
                 }
-                                
+
             </div>
         </div>
-    )    
+    )
 }
 export default HomeScreen;
